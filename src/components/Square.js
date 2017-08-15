8f59c012f62ca596674fdda6ff1850d5937e19b4
shumo.tokinaga <longtime1116@gmail.com>
2017-08-15 11:42:59 +0900

    1st day

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
