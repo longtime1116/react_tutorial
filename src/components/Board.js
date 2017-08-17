f9b8505522a418a664a65d6dfb6cf52c92a48869
shumo.tokinaga <longtime1116@gmail.com>
2017-08-17 19:53:38 +0900

    exercise 3

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,28 +7,69 @@ export default class Board extends React.Component {
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        key={i}
       />
     );
   }
 
   render() {
+    // 配列にpushしていく方法
+    //    const squares2 = []
+    //    for (let i = 0; i < 3; i++) {
+    //      const squares1 = []
+    //      for (let i = 0; i < 3; i++) {
+    //        squares1.push(this.renderSquare(i))
+    //      }
+    //      squares2.push(<div className="board-row">{squares1}</div>)
+    //    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {
+          [0, 1, 2].map((row) => {
+            return (
+              <div className="board-row" key={row}>
+                {
+                  [0, 1, 2].map((column) => {
+                    return this.renderSquare(row * 3 + column)
+                  })
+                }
+              </div>
+            )
+          })
+        }
+
+        {/* 上のやつを省略して簡略化したやつ return と {} を省略している
+        {
+          [0, 1, 2].map((row) =>
+            <div className="board-row" key={row}>
+              {
+                [0, 1, 2].map((column) =>
+                  this.renderSquare(row * 3 + column)
+                )
+              }
+            </div>
+          )
+        }
+        */}
+
+        {/* 羅列
+                <div className="board-row">
+                  {this.renderSquare(0)}
+                  {this.renderSquare(1)}
+                  {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                  {this.renderSquare(3)}
+                  {this.renderSquare(4)}
+                  {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                  {this.renderSquare(6)}
+                  {this.renderSquare(7)}
+                  {this.renderSquare(8)}
+                </div>
+          */}
       </div>
     );
   }
